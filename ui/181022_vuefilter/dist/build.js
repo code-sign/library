47bd06910846401b369d738ab56664d77c0ca67e
code-sign <songssss.k@gmail.com>
2018-11-04 16:46:13 +0900

    add ui vuefilter

--- ui/181022_vuefilter/dist/build.js
+++ ui/181022_vuefilter/dist/build.js
@@ -11328,7 +11328,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
       "dragleave": _vm.FileDragHover,
       "drop": _vm.onFileChange
     }
-  }, [_c('p', [_vm._v("drag on here!")])])]), _vm._v(" "), _c('p', [_c('button', {
+  })]), _vm._v(" "), _c('p', [_c('button', {
     attrs: {
       "id": "originBtn"
     },
