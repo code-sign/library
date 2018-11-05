8cd280f1f7b7fbacc745bb54c3f194e6ee4122d0
code-sign <songssss.k@gmail.com>
2018-11-05 14:17:40 +0900

    ui update ios layout

--- ui/181013_ui_02appstore/js/index.js
+++ ui/181013_ui_02appstore/js/index.js
@@ -2,51 +2,66 @@ $(document).ready(function(){
 
     var CardLeft, CardTop, CardWidth, CardHeight, Card, ScTop;
 
-    $(".card-head").click(function() {
-    Card = $(this).parents('.card');
-    ScTop = $(window).scrollTop();
-    if (!Card.hasClass('is-open')) {
-        $('body, .cards-list').addClass('hidden');
-        $('.is-open').scrollTop(0);
-        
-        CardLeft = Card.offset().left;
-        CardTop = Card.offset().top - ScTop;
-        CardWidth = Card.outerWidth();
-        CardHeight = Card.outerHeight();
+    // scrollBar check
+    var body = $('body');
+    var outerWidth = $(window).outerWidth();
+    var innerWidth = $(window).innerWidth();
+    var scrollWidth = outerWidth - innerWidth;
+    var isScroll = outerWidth > innerWidth;
 
-        console.log(CardLeft,CardTop,CardWidth,CardHeight)
+    $('.card-head').click(function() {
+        Card = $(this).parents('.card');
+        ScTop = $(window).scrollTop();
 
-        Card.css({
-        position:'fixed',
-        zIndex: 3,
-        left: CardLeft + 'px',
-        top: CardTop + 'px',
-        width: CardWidth + 'px',
-        height: CardHeight + 'px'
-        }).parents('.card-holder').css({
-        height: CardHeight + 'px'
-        })
+        if (!Card.hasClass('is-open')) {
 
-        setTimeout(function(){
-        Card.addClass('is-open')
-        }, 100);
+            $('.is-open').scrollTop(0);
+            
+            CardLeft = Card.offset().left;
+            CardTop = Card.offset().top - ScTop;
+            CardWidth = Card.outerWidth();
+            CardHeight = Card.outerHeight();
 
-      }
+            // console.log(CardLeft,CardTop,CardWidth,CardHeight);
+
+            Card.css({
+                position:'fixed',
+                zIndex: 3,
+                left: CardLeft + 'px',
+                top: CardTop + 'px',
+                width: CardWidth + 'px',
+                height: CardHeight + 'px'
+                }).parents('.card-holder').css({
+                height: CardHeight + 'px'
+            });
+
+            setTimeout(function(){
+                Card.addClass('is-open');
+                $('body').addClass('hidden');
+                if(isScroll){
+                    body.css({'paddingRight':scrollWidth+'px'});
+                }
+            }, 100);
+        }
     });
 
-    $(".close").click(function() {
-    $('.card.is-open').removeClass('is-open')
-    setTimeout(function(){
-        Card.css({
-        position:'relative',
-        zIndex: 1,
-        left: 'auto',
-        top: 'auto',
-        width: 'auto',
-        height: 'auto'
-        }).parents('.card-holder').removeAttr('style')
-        $('body, .cards-list').removeClass('hidden')
-    }, 500);
-    })
+    $('.close').click(function() {
+        $('.card.is-open').removeClass('is-open')
+        setTimeout(function(){
+            Card.css({
+            position:'relative',
+            zIndex: 1,
+            left: 'auto',
+            top: 'auto',
+            width: 'auto',
+            height: 'auto'
+            }).parents('.card-holder').removeAttr('style');
+        }, 500);
+
+        $('body').removeClass('hidden');
+        if(isScroll){
+            body.css({'paddingRight':0});
+        }
+    });
 
 });
