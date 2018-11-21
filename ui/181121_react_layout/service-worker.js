758af80a224bbc4dcb1a7036f694e93873a64862
code-sign <songssss.k@gmail.com>
2018-11-21 16:42:11 +0900

    react layout build

--- ui/181121_react_layout/service-worker.js
+++ ui/181121_react_layout/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7dfa05b4364705232da680d64459316.js"
+  "./precache-manifest.346af3c43a366e6adcbf97f40660ae68.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
