f5143fcf18f1ed3d2f19806cf15cfa82dde440b3
code-sign <songssss.k@gmail.com>
2018-11-22 09:15:38 +0900

    edit root path

---
+++ ui/181121_react_layout/precache-manifest.07d296585bfa354ca21b87eea7e99474.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "08aefc3ed1e85ec62d150d4a0283aa80",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-asiana-img.08aefc3e.jpg"
+  },
+  {
+    "revision": "a66908f862acfc7d218f",
+    "url": "/library/ui/181121_react_layout/static/css/main.63fab9e1.chunk.css"
+  },
+  {
+    "revision": "30e04dfcae0629ebe2d4",
+    "url": "/library/ui/181121_react_layout/static/js/1.30e04dfc.chunk.js"
+  },
+  {
+    "revision": "38eec2cea8a2d5125452",
+    "url": "/library/ui/181121_react_layout/static/js/runtime~main.38eec2ce.js"
+  },
+  {
+    "revision": "9b3c16ed48d076ed976246078fa48152",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-aritaum-img.9b3c16ed.jpg"
+  },
+  {
+    "revision": "a66908f862acfc7d218f",
+    "url": "/library/ui/181121_react_layout/static/js/main.a66908f8.chunk.js"
+  },
+  {
+    "revision": "5ecbb0a7143a07a02a228377d9aa5159",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-lottehotel-img.5ecbb0a7.jpg"
+  },
+  {
+    "revision": "58c8733444a80df83d43c18c866eb1e5",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-lottetower-img.58c87334.jpg"
+  },
+  {
+    "revision": "4757e691bb53f5077b3141e6c91543eb",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-namuh-img.4757e691.jpg"
+  },
+  {
+    "revision": "8429c3fb3f42748b15f05ecf67edfae3",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-samsunghospital-img.8429c3fb.jpg"
+  },
+  {
+    "revision": "5855de3dfc75ff24c00cdda16d66ecbf",
+    "url": "/library/ui/181121_react_layout/index.html"
+  }
+];
--- ui/181121_react_layout/service-worker.js
+++ ui/181121_react_layout/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bb716857241cf0ee974f9164c0334433.js"
+  "/library/ui/181121_react_layout/precache-manifest.07d296585bfa354ca21b87eea7e99474.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/library/ui/181121_react_layout/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
