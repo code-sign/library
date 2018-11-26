31f37e07cd5a8dd8b0b5b3495db94b6c5dfc4ff7
code-sign <songssss.k@gmail.com>
2018-11-26 21:50:29 +0900

    update react layout

---
+++ ui/181121_react_layout/precache-manifest.d72db8f66abee1ba4ad3a0c8b8e06661.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b3c16ed48d076ed976246078fa48152",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-aritaum-img.9b3c16ed.jpg"
+  },
+  {
+    "revision": "e4277c1acf2be28f1a14",
+    "url": "/library/ui/181121_react_layout/static/css/main.a04d9828.chunk.css"
+  },
+  {
+    "revision": "b61e9f6a6f32b4ea37a5",
+    "url": "/library/ui/181121_react_layout/static/js/1.b61e9f6a.chunk.js"
+  },
+  {
+    "revision": "38eec2cea8a2d5125452",
+    "url": "/library/ui/181121_react_layout/static/js/runtime~main.38eec2ce.js"
+  },
+  {
+    "revision": "58c8733444a80df83d43c18c866eb1e5",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-lottetower-img.58c87334.jpg"
+  },
+  {
+    "revision": "e4277c1acf2be28f1a14",
+    "url": "/library/ui/181121_react_layout/static/js/main.e4277c1a.chunk.js"
+  },
+  {
+    "revision": "08aefc3ed1e85ec62d150d4a0283aa80",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-asiana-img.08aefc3e.jpg"
+  },
+  {
+    "revision": "5ecbb0a7143a07a02a228377d9aa5159",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-lottehotel-img.5ecbb0a7.jpg"
+  },
+  {
+    "revision": "4757e691bb53f5077b3141e6c91543eb",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-namuh-img.4757e691.jpg"
+  },
+  {
+    "revision": "8429c3fb3f42748b15f05ecf67edfae3",
+    "url": "/library/ui/181121_react_layout/static/media/ourwork-samsunghospital-img.8429c3fb.jpg"
+  },
+  {
+    "revision": "61c205ad3005dc40425e17791c3d5894",
+    "url": "/library/ui/181121_react_layout/index.html"
+  }
+];
---
+++ ui/181121_react_layout/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/library/ui/181121_react_layout/precache-manifest.d72db8f66abee1ba4ad3a0c8b8e06661.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/library/ui/181121_react_layout/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
