d77cb0243914bd5a13042cd586022d251c96916f
code-sign <songssss.k@gmail.com>
2018-12-03 21:18:11 +0900

    react ui update

--- ui/181121_react_layout/service-worker.js
+++ ui/181121_react_layout/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/library/ui/181121_react_layout/precache-manifest.02c8acf7c5c66105109466a3558d21fe.js"
+  "/library/ui/181121_react_layout/precache-manifest.823a1fe534c8ac080bee22b1cdbda1ac.js"
 );
 
 workbox.clientsClaim();
