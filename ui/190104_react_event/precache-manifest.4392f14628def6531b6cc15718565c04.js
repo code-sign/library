1a28260d51486724598bf752bddfed0630db53b4
code-sign <songssss.k@gmail.com>
2019-01-09 10:15:27 +0900

    react polyfill test

---
+++ ui/190104_react_event/precache-manifest.4392f14628def6531b6cc15718565c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f4daae4b2c4a309a8b",
+    "url": "/library/ui/190104_react_event/static/js/runtime~main.31f4daae.js"
+  },
+  {
+    "revision": "d87b323567b8667785de",
+    "url": "/library/ui/190104_react_event/static/js/main.d87b3235.chunk.js"
+  },
+  {
+    "revision": "17d5aafc7b46c80156ac",
+    "url": "/library/ui/190104_react_event/static/js/1.17d5aafc.chunk.js"
+  },
+  {
+    "revision": "d87b323567b8667785de",
+    "url": "/library/ui/190104_react_event/static/css/main.6239568a.chunk.css"
+  },
+  {
+    "revision": "17d5aafc7b46c80156ac",
+    "url": "/library/ui/190104_react_event/static/css/1.7e26351e.chunk.css"
+  },
+  {
+    "revision": "bf97a2cfbc061136b915a8bad2dd5d0b",
+    "url": "/library/ui/190104_react_event/index.html"
+  }
+];
