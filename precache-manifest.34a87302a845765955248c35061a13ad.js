1916e09d36a2b5fdf735d3554a26bbf77cd00539
code-sign <30948029+code-sign@users.noreply.github.com>
2019-10-16 16:35:49 +0900

    refactoring data info & update browserslist info

---
+++ precache-manifest.34a87302a845765955248c35061a13ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072f07c5951101281ff70a891136ad4c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "723e3e4bfcf1e957918d",
+    "url": "./static/css/main.bcda74f9.chunk.css"
+  },
+  {
+    "revision": "17f6acaf353ba15dcf95",
+    "url": "./static/js/2.b3bceded.chunk.js"
+  },
+  {
+    "revision": "723e3e4bfcf1e957918d",
+    "url": "./static/js/main.54f7c6d1.chunk.js"
+  },
+  {
+    "revision": "a1c75af9f1b10f79c955",
+    "url": "./static/js/runtime-main.241003ec.js"
+  },
+  {
+    "revision": "c6d5e1cecd4f8f600d9e70a75097e35d",
+    "url": "./static/media/aa.c6d5e1ce.png"
+  }
+]);
