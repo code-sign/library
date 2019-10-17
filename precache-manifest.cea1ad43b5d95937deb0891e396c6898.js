14326420ccbc1042ec55aafa67d9380bd35f2257
code-sign <30948029+code-sign@users.noreply.github.com>
2019-10-17 13:37:25 +0900

    update a11y color contrast & add meta desc

---
+++ precache-manifest.cea1ad43b5d95937deb0891e396c6898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f53d8f329cb99e7664502c9b1e270b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba85b9921dccbf7abb47",
+    "url": "./static/css/main.5aac62b0.chunk.css"
+  },
+  {
+    "revision": "568532135714f9e2308a",
+    "url": "./static/js/2.dbc35ccb.chunk.js"
+  },
+  {
+    "revision": "ba85b9921dccbf7abb47",
+    "url": "./static/js/main.51c2ce2b.chunk.js"
+  },
+  {
+    "revision": "a1c75af9f1b10f79c955",
+    "url": "./static/js/runtime-main.241003ec.js"
+  },
+  {
+    "revision": "c6d5e1cecd4f8f600d9e70a75097e35d",
+    "url": "./static/media/aa.c6d5e1ce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a214ed5ee9f0c970480ece2650b7671f.js"
+  "./precache-manifest.cea1ad43b5d95937deb0891e396c6898.js"
 );
 
 self.addEventListener('message', (event) => {
